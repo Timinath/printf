8c8ae6215ebaecdae027e3657956cd57510d8830
Timinath <natholwatimileyin@gmail.com>
2022-10-20 09:50:10 +0000

    0

--- _printf.c
+++ _printf.c
@@ -24,7 +24,8 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)											print_buffer(buffer, &buff_ind);
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
 			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
