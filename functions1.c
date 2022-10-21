25831fb4ce33cff650622d63792f48cefc0d75ca
Timinath <natholwatimileyin@gmail.com>
2022-10-21 07:15:42 +0000

    functions1.c

--- functions1.c
+++ functions1.c
@@ -83,7 +83,7 @@ int print_octal(va_list types, char buffer[],
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
+ * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
